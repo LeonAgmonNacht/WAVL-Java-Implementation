409c5f7315397f3a1354b3939322c0788bfd84f6
LeonAgmonNacht <ktuitb@gmail.com>
2017-05-06 18:20:24 +0300

    Adding skeleton code and a Tester class

---
+++ src/Tester.java
@@ -0,0 +1,16 @@
+import WAVLCore.WAVLTree;
+
+/**
+ * Created by Leon on 06/05/2017.
+ */
+public class Tester {
+
+    public static void main(String[] args) {
+
+        WAVLTree tree = new WAVLTree();
+
+        // Preform tests:
+
+
+    }
+}
